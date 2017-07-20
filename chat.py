d5b5d348e2d9a36d8fdd73f507b8e75d86c3a64f
Ilya Lapan <lapan.ilya@gmail.com>
2017-07-20 14:38:37 +0300

    ----

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ import os
 import logging
 import redis
 import gevent
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sockets import Sockets
 
 REDIS_URL = os.environ['REDIS_URL']
