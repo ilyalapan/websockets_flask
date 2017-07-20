b3b3322402bd295d395a1e92b95bfc66565a5991
Ilya Lapan <lapan.ilya@gmail.com>
2017-07-20 16:31:42 +0300

    -----

--- static/js/application.js
+++ static/js/application.js
@@ -8,8 +8,13 @@ if (window.location.protocol == "https:") {
 
 var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
 var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit");
+console.log(ws_scheme + location.host + "/receive")
+console.log(ws_scheme + location.host + "/submit")
 
 inbox.onmessage = function(message) {
+  console.log(message.data)
+  console.log(ws_scheme + location.host + "/receive")
+
   var data = JSON.parse(message.data);
   $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
   $("#chat-text").stop().animate({
