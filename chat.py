d103e29c7101ad4f5fdcb7e8c78a999fa49a7f8b
Ilya Lapan <lapan.ilya@gmail.com>
2017-07-21 17:39:21 +0300

    -----

--- chat.py
+++ chat.py
@@ -81,9 +81,9 @@ def hello():
 @app.route('/open', methods=['POST'])
 def open():
     print('Open')
-    r = Response(response = json.dumps({'status':False}),status=200, mimetype="application/json")
+    #r = Response(response = json.dumps({'status':False}),status=200, mimetype="application/json")
     print(request.get_json())
-    box_id= request.values['']
+    box_id= request.values['box_id']
     if not box_id:
         print('No box ID')
         return r
