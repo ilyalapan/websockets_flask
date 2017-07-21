08a7ce7f9e363a2f6705251900da803b71fd3abf
Ilya Lapan <lapan.ilya@gmail.com>
2017-07-21 17:43:20 +0300

    -----

--- chat.py
+++ chat.py
@@ -82,7 +82,7 @@ def hello():
 def open():
     print('Open')
     #r = Response(response = json.dumps({'status':False}),status=200, mimetype="application/json")
-    print(request.get_json())
+    #print(request.get_json())
     #box_id= request.values['box_id']
     if not box_id:
         print('No box ID')
