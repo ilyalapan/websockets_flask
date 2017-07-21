f4b6333ad3bcba49657f4f38317ab20a8fec830f
Ilya Lapan <lapan.ilya@gmail.com>
2017-07-21 16:34:54 +0300

    -----

--- chat.py
+++ chat.py
@@ -81,7 +81,7 @@ def hello():
 @app.route('/open', methods=['POST'])
 def open():
     print('Open')
-    box_id=string(request.form['box_id'])
+    box_id=request.form['box_id']
     if not box_id:
         print('No box ID')
         return 'False'
